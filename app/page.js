64acd0c1160909a8d5ad53e45f45bc7b4e75158b
ssk607 <sidkhatavkar607@gmail.com>
2026-08-16 17:14:08 +0530

    New projects added and mobile menu header fixed

--- app/page.js
+++ app/page.js
@@ -6,13 +6,19 @@ import Footer from "@/components/Footer/Footer";
 import projects from "./data/project";
 
 export default function Home() {
+  // Chalukya Infra should remain in project.js
+  // but should not appear in the Home slider.
+  const homeProjects = projects.filter(
+    (project) => project.id !== "chalukya-infra"
+  );
+
   return (
     <>
       <main className="home-page">
 
         <HomeHeader />
 
-        <HomeSlider projects={projects} />
+        <HomeSlider projects={homeProjects} />
 
       </main>
 
